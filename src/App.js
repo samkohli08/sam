9cd72a71aabc9ddc4e1ab35d0a79d35335d8c16a
satish kumar <satishkohli08@gmail.com>
2021-08-27 12:35:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>New App</h1>
     </div>
   );
+} */
+/* class App extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+ */
+ class Car extends React.Component {
+  render() {
+    return <h2>I am a {this.props.brand}!</h2>;
+  }
 }
 
-export default App;
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Who lives in my Garage?</h1>
+      <Car brand="Ford" />
+      </div>
+    );
+  }
+}
+ 
+export default Garage;
---
+++ src/Banner.js
@@ -0,0 +1,30 @@
+import first from './images/first.jpg';
+import secnd from './images/secnd.jpg';
+import third from './images/third.jpg';
+function Banner(){
+    return (
+      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={first} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={secnd} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={third} className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+    );
+}
+ 
+export default Banner;
